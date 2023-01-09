7cfb4218b17006e5f08472e83353917b88b9988a
Ksenia Novikova <ksu261086@gmail.com>
2023-01-09 08:52:44 -0500

    change delete button

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -12,7 +12,7 @@ export const Transaction = ({transaction}) => {
         <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
         {transaction.text} <span>{sigh}${Math.abs(transaction.amount)}</span>
         <button onClick={()=> deleteTransaction(transaction.id)}
-        className="delete-btn">x</button>
+        className="delete-button">x</button>
     </li>
     );
 };
